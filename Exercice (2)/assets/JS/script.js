8ac35fccfcff28556fb9c72381fc6314e57d1364
thomas3790 <thomas.audouit@gmail.com>
2020-11-02 12:20:21 +0100

    Initialisation Exercies Partie 1 JS

---
+++ Exercice (2)/assets/JS/script.js
@@ -0,0 +1,7 @@
+// var ou let pour créer variables. Le let se limite à un seul élément, on ne peut pas lui attribuer une valeur à un moment précis.
+var lastName = 'Audouit';
+var firstName = 'Thomas';
+var city = 'Lille'; // Rien ne s'affiche j'ai juste créé les variables.
+// alert('Nom :' + lastName + 'Prénom :' + firstName + 'Ville :' + city);
+alert('Nom : ' + lastName + '\n' + 'Prénom : ' + firstName + '\n' + 'Ville : ' + city);
+// + '\n' + pour le retour à la ligne.
